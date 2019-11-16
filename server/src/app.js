90881acb9465b8c3711877f9bf6b5b3162f1d761
dr10n9 <dr10n9@gmail.com>
2019-11-16 18:30:44 +0200

    added cors and bodyParser

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 
 const config = require('./config');
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+
+app.use('/api', routes.Api);
+app.use('/films', routes.Film);
 
 app.listen(config.PORT, async (err) => {
     if(err) console.log(err);
@@ -19,4 +31,4 @@ app.listen(config.PORT, async (err) => {
             console.log(error);
         }
     }
-})
+});
