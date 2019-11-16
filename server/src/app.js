f88d073ce3642c7e08070142b4776a4480c0dd7b
dr10n9 <dr10n9@gmail.com>
2019-11-16 16:32:06 +0200

    mongoose connection

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const config = require('./config');
 
 
 
-app.listen(config.PORT, (err) => {
+app.listen(config.PORT, async (err) => {
     if(err) console.log(err);
     else {
         console.log(`listening on ${config.PORT}`);
-        // TODO mongoose connection;
+        try {
+            let connection = mongoose.connect(config.DATABASE_URI, {
+                useNewUrlParser: true
+            });
+            console.log('connected');
+        } catch (error) {  
+            console.log(error);
+        }
     }
 })
