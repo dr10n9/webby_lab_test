907d89ce272ec4b68c27a561a80fb3ff0c39cce3
dr10n9 <dr10n9@gmail.com>
2019-11-19 00:30:56 +0200

    redux removed from FilmsList component

--- client/src/components/filmsList.js
+++ client/src/components/filmsList.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import axios from '../utils/axios';
 import { Link } from 'react-router-dom';
-import { ListGroup } from 'react-bootstrap';
-import { MDBBtn } from 'mdbreact';
-import { connect } from 'react-redux';
-
-import { setFilms, addFilms, clearFilms } from '../store/actions/filmsList';
+import { ListGroup, Row, Col, Button, InputGroup } from 'react-bootstrap';
 
 class FilmsList extends React.Component {
     constructor(props) {
@@ -19,6 +15,7 @@ class FilmsList extends React.Component {
     }
 
     componentDidMount = () => {
+        console.log('mount');
         this.loadFilms(1);
     }
 
@@ -27,30 +24,24 @@ class FilmsList extends React.Component {
             sort: event.target.checked,
             page: 1
         }, () => {
-            this.loadFilms(this.loadFilms(this.state.page));
+            this.loadFilms(this.state.page);
         });
     }
 
     loadFilms = (page, more = false) => {
         axios.get(`/films/?page=${page}&sort=${this.state.sort}`)
             .then(data => {
-                let tmp = data.data.docs.map(el => {
-                    return (
-                        <div key={el.film_id}>
-                            <ListGroup.Item>
-                                <Link to={"/films/" + el.film_id}>
-                                    Name: {el.name}
-                                </Link>
-                            </ListGroup.Item>
-                        </div>
-                    )
-                });
-                if (more) tmp = this.state.films.concat(tmp);
-                this.setState({
-                    films: tmp,
-                    page: data.data.page,
-                    totalPages: data.data.pages
-                });
+                if (more) {
+                    this.setState({
+                        films: this.state.films.concat(data.data.docs)
+                    });
+                } else {
+                    this.setState({
+                        films: data.data.docs,
+                        page: data.data.page,
+                        totalPages: data.data.pages
+                    });
+                }
             })
             .catch(err => {
                 console.log(err);
@@ -68,40 +59,75 @@ class FilmsList extends React.Component {
     canLoadMore = () => {
         if (this.state.page < this.state.totalPages) {
             return (
-                <MDBBtn outline onClick={this.handleClick}>
+                <Button variant="outline-primary" onClick={this.handleClick}>
                     Load more
-                </MDBBtn>
+                </Button>
             );
         } else {
             return '';
         }
     }
 
+    resetList = (event) => {
+        this.setState({
+            more: false,
+            sort: false
+        })
+        this.loadFilms(1);
+    }
+
     render() {
         return (
             <div>
-                Sort  <input type="checkbox" type="checkbox" onChange={this.handleSort} />
+                <Row>
+                    <Col>
+                        <InputGroup>
+                            <InputGroup.Checkbox onChange={this.handleSort} />
+                            <InputGroup.Text id="basic-addon2">Sort</InputGroup.Text>
+                        </InputGroup>
+                    </Col>
+                    <Col>
+                        <Button variant="outline-info" as={Link} to="/form">create form</Button>
+                    </Col>
+                </Row>
                 <ListGroup>
-                    {this.state.films}
+                    {this.state.films.map(el => {
+                        return (
+                            <div key={el.film_id}>
+                                <ListGroup.Item>
+                                    <Link to={"/films/" + el.film_id}>
+                                        Name: {el.name}
+                                    </Link>
+                                </ListGroup.Item>
+                            </div>
+                        )
+                    })}
                 </ListGroup>
                 {this.canLoadMore()}
+                <Button variant="outline-primary" onClick={this.resetList}>Reset list</Button>
             </div>
         );
     }
 }
 
-const mapToStateProps = store => {
-    return {
-        filmsList: store.filmsList
-    }
-}
 
-const mapDispatchToActions = dispatch => {
-    return {
-        setFilmsActions: films => dispatch(setFilms(films)),
-        addFilmsAction: films => dispatch(addFilms(films)),
-        clearFilmsAction: () => dispatch(clearFilms())
-    }
-}
+export default FilmsList;   
 
-export default connect(mapToStateProps, mapDispatchToActions)(FilmsList);
+
+// const mapToStateProps = store => {
+//     return {
+//         filmsList: store.filmsList
+//     }
+// }
+
+// const mapDispatchToActions = dispatch => {
+//     return {
+//         setFilmsActions: films => dispatch(setFilms(films)),
+//         addFilmsAction: films => dispatch(addFilms(films)),
+//         clearFilmsAction: () => dispatch(clearFilms()),
+//         setPageAction: (page) => dispatch(setPage(page)),
+//         setPagesAction: (pages) => dispatch(setPages(pages))
+//     }
+// }
+
+// export default connect(mapToStateProps, mapDispatchToActions)(FilmsList);
