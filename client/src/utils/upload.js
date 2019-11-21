4b6460cba0260e756b60ba1e3207501191985094
dr10n9 <dr10n9@gmail.com>
2019-11-21 17:07:50 +0200

    modal for created films count

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import axios from './axios';
+import { Button, Modal } from "react-bootstrap";
 
 class Upload extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedFile: null
+            selectedFile: null,
+            modal: false,
+            count: ''
         }
     }
 
@@ -17,12 +20,19 @@ class Upload extends React.Component {
         });
     }
 
-    onClickHandler = () => {
+    onClickHandler = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
         let data = new FormData();
         data.append('file', this.state.selectedFile);
         axios.post('/upload', data, {})
             .then(data => {
                 console.log(data);
+                this.setState({
+                    modal: true,
+                    count: data.data.created
+                });
             })
             .catch(err => {
                 console.log(err);
@@ -44,6 +54,24 @@ class Upload extends React.Component {
                     <input type="file" name="file" onChange={this.onChangeHandler} />
                     <button onClick={this.onClickHandler}>Upload</button>
                 </form>
+                <Modal
+                    show={this.state.modal}
+                    dialogClassName="modal-90w"
+                    // onHide={() => this.setState({ modal: false })}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            Response
+                            {/* Oops! You got {this.state.error.code} error! */}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{this.state.count} created, check DevTools for info</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => this.setState({modal: false})}>
+                            Ok
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
     }
