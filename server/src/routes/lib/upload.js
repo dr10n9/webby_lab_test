9297607b4396cf786ef23c9cdf472aa924786bc6
dr10n9 <dr10n9@gmail.com>
2019-11-26 19:30:50 +0200

    file processing done without saving

--- server/src/routes/lib/upload.js
+++ server/src/routes/lib/upload.js
@@ -1,93 +1,71 @@
-const IncomingForm = require('formidable').IncomingForm;
 const router = require('express').Router();
-const fs = require('fs');
 const papaParse = require('papaparse');
 const model = require('../../models').Film;
 const sortActors = require('../../config').sortActors;
 
+const muter = require('multer');
+const upload = muter();
 
-router.post('/', (req, res) => {
-    let form = new IncomingForm();
-    form.uploadDir = __dirname;
-    form.on('file', (field, file) => {
-        console.log(file.path);
-        fs.readFile(file.path, 'utf-8', (err, data) => {
-            papaParse.parse(data, {
-                complete: async (data) => {
-                    console.log(data);
-                    let response = [];
-                    for (let film of data.data) {
-                        let tmp = {
-                            name: film.name.trim(),
-                            format: film.format.trim(),
-                            actors: sortActors(film.actors.split(',').map(el => el.trim())),
-                            yearOfIssue: film.yearOfIssue
-                        }
+router.post('/', upload.single('file'), (req, res) => {
+    console.log(req.file.buffer.toString());
+    if(req.file == undefined) return res.status(400).json({
+        err: 'no file'
+    });
+
+    papaParse.parse(req.file.buffer.toString(), {
+        complete: async (data) => {
+            console.log(data);
+            let response = [];
+            for (let film of data.data) {
+                let tmp = {
+                    name: film.name.trim(),
+                    format: film.format.trim(),
+                    actors: sortActors(film.actors.split(',').map(el => el.trim())),
+                    yearOfIssue: film.yearOfIssue
+                }
+                try {
+                    let check = await model.findOne(tmp);
+                    if (check != undefined) {
+                        response.push({
+                            film: check,
+                            created: false,
+                            reason: 'exists'
+                        });
+                        console.log(film, 'exists');
+                    } else {
+                        tmp.yearOfIssue = film.yearOfIssue
                         try {
-                            let check = await model.findOne(tmp);
-                            if(check != undefined) {
-                                response.push({
-                                    film: check,
-                                    created: false,
-                                    reason: 'exists'
-                                });
-                                console.log(film, 'exists');
-                            } else {
-                                tmp.yearOfIssue = film.yearOfIssue
-                                try {
-                                    let res = await model.create(tmp);
-                                    response.push({
-                                        film: res,
-                                        created: true
-                                    });
-                                    console.log(film, 'created');
-                                } catch (error) {
-                                    return res.status(500).json({
-                                        err: error
-                                    });
-                                }
-                            }
-                        } catch (err) {
+                            let res = await model.create(tmp);
+                            response.push({
+                                film: res,
+                                created: true
+                            });
+                            console.log(film, 'created');
+                        } catch (error) {
                             return res.status(500).json({
-                                err: err
+                                err: error
                             });
                         }
                     }
-                    fs.unlink(file.path, (err) => {
-                        if (err) console.log(err);
-                        else {
-                            console.log('deleted');
-                            return res.status(200).json({
-                                result: response,
-                                created: `${response.filter(el => el.created).length}/${response.length}`
-                                // mes: 'processed'
-                            });
-                        }
-                    })
-                },
-                error: (err) => {
-                    console.log(err);
+                } catch (err) {
                     return res.status(500).json({
-                        err: 'PapaParse error: ' + err
+                        err: err
                     });
-                },
-                header: true,
-                dynamicTyping: true,
+                }
+            }
+            return res.status(200).json({
+                result: response,
+                created: `${response.filter(el => el.created).length}/${response.length}`
             });
-        });
-    });
-
-    form.on('end', () => { });
-
-    form.on('error', (err) => {
-        console.log(err);
-    });
-
-    form.on('aborted', () => {
-        console.log('aborted');
-    });
-
-    form.parse(req);
+        },
+        error: (err) => {
+            console.log(err);
+            return res.status(500).json({
+                err: 'PapaParse error: ' + err
+            });
+        },
+        header: true,
+        dynamicTyping: true,
+    })
 });
-
 module.exports.router = router;
