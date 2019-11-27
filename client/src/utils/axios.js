f654b3f88c59e80dc40335094b8e3fbbae449108
dr10n9 <dr10n9@gmail.com>
2019-11-27 11:55:43 +0200

    baseurl changed

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: 'http://288cdc41.eu.ngrok.io/api'
-    baseURL: 'http://localhost:2323/api'
+    // baseURL: 'http://localhost:2323/api'
+    baseURL: 'http://murmuring-citadel-92335.herokuapp.com/api'
 });
